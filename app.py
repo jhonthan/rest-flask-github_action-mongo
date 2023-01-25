7d5a29b349e26dcf43e7f359833d2a02ff16242b
Jonathan Luiz <luiz_jal@yahoo.com.br>
2023-01-25 23:28:35 +0100

    fix connection refused mongo

--- app.py
+++ app.py
@@ -1,15 +1,33 @@
 
 from flask import Flask
 from flask_restful import Resource, Api
+from flask_mongoengine import MongoEngine
 
 app = Flask(__name__)
-api = Api(app)
 
+app.config['MONGODB_SETTINGS'] = {
+    'db': 'users',
+    'port': '27017',
+    'host': 'mongodb',    
+    'username': 'admin',
+    'password': 'admin'
+}
+
+api = Api(app) #Inicializa API
+db = MongoEngine(app) #Inicializa o banco
+
+#Declaracao da classe conectando ao Banco - MongoDB - NoSQL
+class UserModel(db.Document):
+    cpf = db.StringField(required=True, unique=True)
+    first_name = db.StringField(required=True)
+    last_name = db.StringField(required=True)
+    email = db.EmailField(required=True)
+    birth_date = db.DateTimeField(required=True)   
 
 class Users(Resource):
     def get(self):
+        #return jsonify(UserModel.objects())
         return {"message": "user 1"}
-
 
 class User(Resource):
     def post(self):
@@ -18,7 +36,7 @@ class User(Resource):
     def get(self, cpf):
         return {"message": "CPF"}
 
-
+#Endpoints
 api.add_resource(Users, '/users')
 api.add_resource(User, '/user', '/user/<string:cpf>')
 
